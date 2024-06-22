4c8e7de8008fdb74904fc5bcb611c285b2ab9c28
JohnnyMoneJohnny <158449921+JohnnyMoneJohnny@users.noreply.github.com>
2024-06-22 15:35:49 +0100

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,9 +1,16 @@
-function sendMail(){
-  let parms = {
-    name : document.getElementById('name').value,
-    email : document.getElementById('email').value,
-    message : document.getElementById('message').value
-  }
-  
-  emailjs.send("contact_services","contact_form", parms).then(alert("Email Succesfully Sent"))
+function sendMail() {
+    let params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    };
+
+    emailjs.send("contact_services", "contact_form", params)
+        .then(function(response) {
+            alert("Email Successfully Sent");
+            console.log("SUCCESS!", response.status, response.text);
+        }, function(error) {
+            alert("Oops, something went wrong. Please try again later.");
+            console.error("FAILED...", error);
+        });
 }
